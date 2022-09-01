7dd929883a1e8b0233114ecb2915c1548f2e2364
sthakrishnakumar <sthakrishnakumar@gmail.com>
2022-09-01 11:23:47 +0545

    get user repository added

---
+++ lib/features/users/repository/get_user_repository.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+import 'package:onviro/core/app_error.dart';
+import 'package:onviro/features/users/data/data_source/get_user_data_source.dart';
+import 'package:onviro/features/users/data/models/get_user_request_model.dart';
+
+abstract class GetUserRepository {
+  Future<Either<AppError, List<GetUsersModel>>> getUsers();
+}
+
+class GetUserRepositoryImpl implements GetUserRepository {
+  GetUserDataSource getUserDataSource;
+  GetUserRepositoryImpl({
+    required this.getUserDataSource,
+  });
+
+  @override
+  Future<Either<AppError, List<GetUsersModel>>> getUsers() async {
+    try {
+      final result = await getUserDataSource.getUsers();
+      return right(result);
+    } on DioError catch (e) {
+      return Left(
+        AppError(message: e.message),
+      );
+    }
+  }
+}
